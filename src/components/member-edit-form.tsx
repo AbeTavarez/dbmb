9ca298871bdcec418d430dcff3f8df6880cd0ac7
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-03-18 23:18:11 -0400

    edit member form

---
+++ src/components/member-edit-form.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Member } from "@/types";
+import { ChangeEvent, useState } from "react";
+
+interface MemberEditFormProps {
+  member: Member;
+}
+
+export default function MemberEditForm(props: MemberEditFormProps) {
+  const [member, setMember] = useState(props.member);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMember({...member, [e.target.name]: e.target.value});
+  }
+  return (
+    <form>
+      <div className="flex flex-col gap-3 max-w-xl mx-auto bg-slate-300 p-5 rounded">
+        <h3 className="font-bold m-3">Edit Member</h3>
+
+        <div className="flex gap-5">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border rounded p-2 w-full"
+            defaultValue={member.name}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="avatar">Avatar</label>
+          <input
+            type="text"
+            name="avatar"
+            id="avatar"
+            className="border rounded p-2 w-full"
+            defaultValue={member.avatar}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        <button type="submit" className="rounded bg-green-500 p-2 w-full text-white font-bold">
+          Update
+        </button>
+      </div>
+    </form>
+  );
+}
